b70fcc1082c8ee2813b27949b5f136e52538d8f7
tayanPinto <taylanlaflamme@gmail.com>
2024-11-11 08:24:03 -0300

    Icones e BG

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { FaUser, FaLock } from 'react-icons/fa';
 
 const Login = () => {
   return (
     <div className="container">
       <form>
         <h1>Acesse o sistema</h1>
-        <input type="email" placeholder="e-mail" />
-        <input type="password" placeholder="senha" />
+        <div>
+          <input type="email" placeholder="e-mail" />
+          <FaUser className="icon" />
+        </div>
+        <div>
+          <input type="password" placeholder="senha" />
+          <FaLock className="icon" />
+        </div>
         <button>Entrar</button>
       </form>
     </div>
