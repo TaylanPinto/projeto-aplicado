74e82a2fea5d20b9eb0e38bebfe5a01f9960fc01
tayanPinto <taylanlaflamme@gmail.com>
2024-11-12 09:05:44 -0300

    Salvando login

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { FaUser, FaLock } from 'react-icons/fa';
+import { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Envio');
+  };
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
         <div>
-          <input type="email" placeholder="e-mail" />
+          <input
+            type="email"
+            placeholder="e-mail"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <FaUser className="icon" />
         </div>
         <div>
-          <input type="password" placeholder="senha" />
+          <input
+            type="password"
+            placeholder="senha"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <FaLock className="icon" />
         </div>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
+        </div>
+
         <button>Entrar</button>
       </form>
     </div>
