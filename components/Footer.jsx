7c80c29f201da90158cc66b8b94b9e38f4b90323
Rafael Altarriba <rafael_altarriba@estudante.sc.senai.br>
2024-12-02 11:22:51 -0300

    Footer de acordo com atividade

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,18 @@
+import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <footer> Projeto Aplicado</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <p className="footer-projeto">Projeto Aplicado</p>
+        <div className="footer-contato">
+          <p>Contato: (48) 3239-5801</p>
+          <p>Email: <a href="mailto:faleconosco@fiesc.com.br" className="footer-link">faleconosco@fiesc.com.br</a></p>
+        </div>
+      </div>
+    </footer>
+  );
 }
+
 export default Footer;
